cc1f1bcca6f54e94f8e0d0d9ae901415242ea6a5
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2023-09-29 18:53:12 +0300

    v290920231853

--- code/dataframe.py
+++ code/dataframe.py
@@ -4,5 +4,5 @@ import math
 file = pandas.read_csv("csv/IMDB-Movie-Data.csv")
 check = file.isnull().sum() #checking NaN values
 cleared = file.dropna() #clearing file for NaN values
-dataframe = cleared.head(10)
+dataframe = cleared
 #dataframe = cleared[cleared["Revenue (Millions)"] > 400]
