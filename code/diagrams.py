a11815037fd3643bc64fa6b0e819b308a2c6c803
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2023-09-29 18:29:25 +0300

    v290920231829

---
+++ code/diagrams.py
@@ -0,0 +1,22 @@
+from dataframe import*
+import pandas
+import math
+import plotly.express as exp
+
+#друга гіпотеза: чи залежить режисер(Director) фільму на голоси(Votes)?
+dataframe = dataframe.sort_values(by="Votes")
+dv = exp.bar(dataframe, x="Director", y="Votes", title="DirectorVotes")
+dv.show()
+
+#третє: скільки заробив 3 найбільш довготривалі фільми в IMDb?
+dataframe = dataframe.sort_values(by="Runtime (Minutes)")
+dataframe = dataframe.tail(3)
+rr = exp.pie(dataframe, names = "Runtime (Minutes)", values = "Revenue (Millions)", title="MostRuntimeRevenue")
+rr.show()
+
+#Четверте: топ 3 найпопулярніших фільмів 2016 року
+dataframe = dataframe.sort_values(by="Rating")
+dataframe = dataframe[dataframe["Year"] == 2016]
+dataframe = dataframe.tail(3)
+ry = exp.bar(dataframe, x="Title", y="Rating", title="Top-3 most pupular films 2016")
+ry.show()
