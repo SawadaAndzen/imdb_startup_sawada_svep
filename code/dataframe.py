a6b8b2581a187d03526700174a18d182c21d3396
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2023-09-29 18:31:08 +0300

    v290920231830

--- code/dataframe.py
+++ code/dataframe.py
@@ -1,7 +1,7 @@
 import pandas
 import math
 
-file = pandas.read_csv(".csv/IMDB-Movie-Data.csv")
+file = pandas.read_csv("csv/IMDB-Movie-Data.csv")
 check = file.isnull().sum() #checking NaN values
 cleared = file.dropna() #clearing file for NaN values
 dataframe = cleared.head(10)
